02b8b173b0c915785034a17e301ece1d10c0cd60
Hao Duong <hao.duong11@gmail.com>
2023-06-27 17:02:01 +1000

    Add store layout with data fetching from Shopify Storefront API

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,46 @@
-export default function Home() {
+import { Header, Footer, CategoryFilter } from "@/components";
+import { storefront } from "@/utils";
+
+export default async function Home(props) {
+
+  const { data } = await storefront(productsQuery);
+
   return (
-    <main>
-      <h1>Hello, world!</h1>
-    </main>
+    <>
+      <h1>{props.hi}</h1>
+      <Header></Header>
+      <main>
+        <CategoryFilter data={data}></CategoryFilter>
+      </main>
+      <Footer></Footer>
+    </>
   )
 }
+
+const gql = String.raw
+
+const productsQuery = gql`
+query Products {
+  products(first:5) {
+    edges {
+      node {
+        title
+        handle
+        priceRange {
+          minVariantPrice {
+            amount
+          }
+        }
+        images(first:1) {
+          edges {
+            node {
+              url
+              altText
+            }
+          }
+        }
+      }
+    }
+  }
+}
+`
---
+++ src/components/footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <footer>
+            <div className="flex p-8 mx-16 border-t border-gray-200">
+                <div className="text-xs text-gray-500">Hao Duong</div>
+            </div>
+        </footer>
+    )
+}
